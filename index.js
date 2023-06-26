45750e6362d161bc630b4abf8d3a883f024feba1
akira-cn <19511344@qq.com>
2023-06-26 15:42:59 +0800

    add conditions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-import Table from "./lib/table.js";
+import Database from "./lib/db.js";
 
-const t = new Table('abc');
+const d = new Database();
+const t = d.table('abc');
 console.log(t.records);
 
-const res = await t.save({foo: 'bar2'});
-console.log(t.where({foo: 'bar2'}).findOne());
+// const res = await t.save({foo: 'bar3', score: 100});
+console.log(t.where({foo: 'bar3', score: d.not(d.lt(90))}).findOne());
 
 // const res = await t.save({foo: 'bar'});
 // console.log(res, t.records);
