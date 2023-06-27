0028909f8cc0270162dee2a4bbc536f72896559a
akira-cn <19511344@qq.com>
2023-06-27 14:56:08 +0800

    add locker & metadata

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import Database from "./lib/db.js";
 
 const d = new Database();
 const t = d.table('abc');
-console.log(t.records);
+console.log(await t.getRecords());
 
-// const res = await t.save({foo: 'bar3', score: 100, y : new Date(), z: [1, 2, 3], x: null, w: /a/g});
-const rec = t.where({foo: 'bar3', score: d.not(d.lt(90))}).and({flag: d.type('null')}).findOne();
+const res = await t.save({foo: 'bar3', score: 100, y : new Date(), z: [1, 2, 3], x: null, w: /a/g});
+const rec = await t.where({foo: 'bar3', score: d.not(d.lt(90))}).and({flag: d.exists(false)}).findOne();
 console.log(rec);
 
 // const res = await t.save({foo: 'bar'});
