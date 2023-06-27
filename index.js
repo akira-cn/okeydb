24a4ac8adb2d27b1a591fe6090aa82e6c3389002
akira-cn <19511344@qq.com>
2023-06-27 11:44:42 +0800

    more operators

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const d = new Database();
 const t = d.table('abc');
 console.log(t.records);
 
-// const res = await t.save({foo: 'bar3', score: 100});
-console.log(t.where({foo: 'bar3', score: d.not(d.lt(90))}).findOne());
+// const res = await t.save({foo: 'bar3', score: 100, y : new Date(), z: [1, 2, 3], x: null, w: /a/g});
+const rec = t.where({foo: 'bar3', score: d.not(d.lt(90))}).and({flag: d.type('null')}).findOne();
+console.log(rec);
 
 // const res = await t.save({foo: 'bar'});
 // console.log(res, t.records);
