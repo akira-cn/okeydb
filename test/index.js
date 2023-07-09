b5c1fcfdcdc18da87754cf0aeba174d2c0e10ed5
akira-cn <akira.cn@gmail.com>
2023-07-09 10:52:42 +0800

    remove unused args & prepare to add schema

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
 import AirDB from '../index.js';
 
+import {sjs, attr} from 'slow-json-stringify';
+
 const db = new AirDB();
 // const personTable = db.table('person');
 // for(let i = 0; i < 10; i++) {
@@ -16,17 +18,33 @@ function randomScore() {
 const bigTable = db.table('bigtable');
 
 const students = [];
-for(let i = 0; i < 1000; i++) {
-  students.push({name: `student${i}`, score: randomScore()});
+for(let i = 0; i < 3000000; i++) {
+  students.push({name: `student${i}`, score: randomScore(), createdAt: new Date(), updatedAt: new Date()});
 }
 
-await bigTable.save(students);
+const stringify = sjs({
+  students: attr('array', sjs({
+    name: attr('string'),
+    score: attr('number'),
+    createdAt: attr('string', (value) => value.toISOString()),
+    updatedAt: attr('string', (value) => value.toISOString()),
+  }))
+});
 
-const res = await bigTable.where().find();
-console.log(res.length);
+console.time('stringify');
+// JSON.stringify({students});
+const r = stringify({students});
+console.timeEnd('stringify');
+
+// console.log(r);
+
+// await bigTable.save(students);
+
+// const res = await bigTable.where().find();
+// console.log(res.length);
 
 
-// await bigTable.where().delete();
+// // await bigTable.where().delete();
 
-const result = await bigTable.where({score: db.gt(60)}).find();
-console.log(result.length);
+// const result = await bigTable.where({score: db.gt(60)}).find();
+// console.log(result.length);
