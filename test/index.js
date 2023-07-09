fbdd37b5c77a911624fa5ec77c57f7ff7e12aa4f
akira-cn <akira.cn@gmail.com>
2023-07-09 14:39:54 +0800

    optimize deletion performance

--- test/index.js
+++ test/index.js
@@ -1,50 +1,84 @@
 import AirDB from '../index.js';
 
-import {sjs, attr} from 'slow-json-stringify';
+// import {sjs, attr} from 'slow-json-stringify';
 
 const db = new AirDB();
-// const personTable = db.table('person');
-// for(let i = 0; i < 10; i++) {
-//   await personTable.save({name: `student${i}`, score: 30 + Math.floor(Math.random() * 70)});
-// }
-// await personTable.where({name: 'akira'}).delete();
-// const result = await personTable.where({score: db.gt(60)}).sort({score: -1}).projection({_id: 0}).find();
+const personTable = db.table('bigtable');
+
+console.time('delete0');
+const res = await personTable.where().delete();
+console.timeEnd('delete0');
+console.log(res);
+
+const persons = [];
+for(let i = 0; i < 1000000; i++) {
+  persons.push({name: `student${i}`, score: 30 + Math.floor(Math.random() * 70)});
+}
+
+console.time('save0');
+await personTable.save(persons);
+console.timeEnd('save0');
+
+console.time('find0');
+console.log((await personTable.where().find()).length);
+console.timeEnd('find0');
+
+// console.time('save1');
+// await personTable.save({name: 'akira', score: 111});
+// console.timeEnd('save1');
+// console.log((await personTable.where().find()).length);
+
+
+// const result = await personTable.where({}).sort({score: -1}).projection({_id: 0}).find();
 // console.table(result);
 
-function randomScore() {
-  return Math.floor(100 * Math.random());
-}
+// function randomScore() {
+//   return Math.floor(100 * Math.random());
+// }
 
-const bigTable = db.table('bigtable');
+// const bigTable = db.table('bigtable');
 
-const students = [];
-for(let i = 0; i < 3000000; i++) {
-  students.push({name: `student${i}`, score: randomScore(), createdAt: new Date(), updatedAt: new Date()});
-}
+// const students = [];
+// for(let i = 0; i < 20000; i++) {
+//   students.push({
+//     name: `student${i}`,
+//     score: randomScore(),
+//     createdAt: new Date(),
+//     updatedAt: new Date(),
+//     abc: /a/g,
+//   });
+// }
 
-const stringify = sjs({
-  students: attr('array', sjs({
-    name: attr('string'),
-    score: attr('number'),
-    createdAt: attr('string', (value) => value.toISOString()),
-    updatedAt: attr('string', (value) => value.toISOString()),
-  }))
-});
+// RegExp.prototype.toJSON = function() {
+//   return {type: 'RegExp', source: this.source, flags: this.flags};
+// };
 
-console.time('stringify');
+// const stringify = sjs({
+//   students: attr('array', sjs({
+//     name: attr('string'),
+//     score: attr('number'),
+//     createdAt: attr('string', (value) => value.toISOString()),
+//     updatedAt: attr('string', (value) => value.toISOString()),
+//     abc: attr('string'),
+//   }))
+// });
+
+// console.time('stringify');
 // JSON.stringify({students});
-const r = stringify({students});
-console.timeEnd('stringify');
 
-// console.log(r);
+// // const r = stringify({students});
 
-// await bigTable.save(students);
+// console.timeEnd('stringify');
 
-// const res = await bigTable.where().find();
-// console.log(res.length);
+// // console.log(r);
+
+// // await bigTable.save(students);
+
+// // const res = await bigTable.where().find();
+// // console.log(res.length);
 
 
-// // await bigTable.where().delete();
+// // // await bigTable.where().delete();
 
-// const result = await bigTable.where({score: db.gt(60)}).find();
-// console.log(result.length);
+// // const result = await bigTable.where({score: db.gt(60)}).find();
+// // console.log(result.length);
